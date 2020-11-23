e4c09c2563f8fab904ccb3ad4b9172c3b59415f0
cexbrayat <cedric@ninja-squad.com>
2020-11-23 08:37:36 +0100

    repro: vuedx comment in template

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <!-- comment -->
+    <div v-for="user in users" :key="user">{{ user }}</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    return { users: ['jb', 'agnes'] };
   }
 });
 </script>
